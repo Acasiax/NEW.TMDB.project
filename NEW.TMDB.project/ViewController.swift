8955ecc11185b739b0eb25e584f62a3291c12658
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-06-26 14:25:16 +0900

    Initial Commit

---
+++ NEW.TMDB.project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NEW.TMDB.project
+//
+//  Created by 이윤지 on 6/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
